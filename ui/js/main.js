fb6776cd32149084d3ae8f961ef2391c85ed13bd
Nwabuzor C. Obiora <nwabuzor.obiora@gmail.com>
2018-09-09 23:47:39 +0100

    [feature-#160366698] create modal login form

--- ui/js/main.js
+++ ui/js/main.js
@@ -2,6 +2,9 @@ const banner = document.getElementById("banner");
 const signup = document.getElementById("signup");
 const signupModal = document.getElementsByClassName("signup-modal")[0];
 const signupBox = document.getElementsByClassName("signup-box")[0];
+const login = document.getElementById("signin");
+const loginModal = document.getElementsByClassName("login-modal")[0];
+const loginBox = document.getElementsByClassName("login-box")[0];
 
 let count = 0, nextCount = 1, format = "png";
 
@@ -18,19 +21,30 @@ function slideshow() {
     }
 }
 
-setInterval(slideshow, 60000);
+setInterval(slideshow, 20000);
 
 //Open modal for sign up
 signup.addEventListener("click", () => {
     signupModal.style.display = "block";
 });
 
-// close modal onclick outside
-function closeModal (m) {
+// close modal onclick outside for signup
+function closeSignupModal (m) {
     if (m.target === signupModal) {
         signupModal.style.display = "none";
     }
 }
-window.addEventListener("click", closeModal);
+window.addEventListener("click", closeSignupModal);
 
-//Close modal on click x
+//Open modal for Login
+login.addEventListener("click", () => {
+    loginModal.style.display = "block";
+});
+
+// close modal onclick outside for Login
+function closeLoginModal(e) {
+    if (e.target === loginModal) {
+        loginModal.style.display = "none";
+    }
+}
+window.addEventListener("click", closeLoginModal);
