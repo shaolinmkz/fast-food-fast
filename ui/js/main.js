0323fb082fe91c28d8b75e1701ad000c336b6b61
Nwabuzor C. Obiora <nwabuzor.obiora@gmail.com>
2018-09-09 21:11:44 +0100

    [feature-#10366618] create sign up form

--- ui/js/main.js
+++ ui/js/main.js
@@ -1,9 +1,36 @@
-window.addEventListener("load", function(){
-    if(this.window.innerWidth < "768px") {
-       const fff =  document.getElementsByClassName("fastfoodfast")[0];
-        fff.setAttribute("src", "../images/fastfoodfast1.png");
-    } else if (this.window.innerWidth >= "768px") {
-        const fff = document.getElementsByClassName("fastfoodfast")[0];
-        fff.setAttribute("src", "../images/fastfoodfast.png");
+const banner = document.getElementById("banner");
+const signup = document.getElementById("signup");
+const signupModal = document.getElementsByClassName("signup-modal")[0];
+const signupBox = document.getElementsByClassName("signup-box")[0];
+
+let count = 0, nextCount = 1, format = "png";
+
+function slideshow() {
+    if (nextCount === 12) {
+        format = "jpg";
+    } else {
+        format = "png";
     }
-})
+    banner.setAttribute("src", `./images/food${nextCount}.${format}`);
+    nextCount++;
+    if (nextCount === 14) {
+        nextCount = 1;
+    }
+}
+
+setInterval(slideshow, 60000);
+
+//Open modal for sign up
+signup.addEventListener("click", () => {
+    signupModal.style.display = "block";
+});
+
+// close modal onclick outside
+function closeModal (m) {
+    if (m.target === signupModal) {
+        signupModal.style.display = "none";
+    }
+}
+window.addEventListener("click", closeModal);
+
+//Close modal on click x
