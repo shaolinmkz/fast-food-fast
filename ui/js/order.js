68f297e29e8306c1ee9464c98f1f4761df1480db
Nwabuzor C. Obiora <nwabuzor.obiora@gmail.com>
2018-09-20 08:31:42 +0100

    remove animation and porpulate page

--- ui/js/order.js
+++ ui/js/order.js
@@ -1,51 +1,71 @@
 const orderItem = document.getElementsByClassName("order-item");
 const orderModal = document.getElementsByClassName("order-modal")[0];
-const orderBox = document.getElementsByClassName("order-box")[0];
 const close = document.getElementById("close");
 const banner = document.getElementById("bannerImg");
 const logo = document.getElementById("logo");
 
-let count = 0, nextCount = 1, format = "png";
+let nextCount = 1, format = "png";
 
-function slideshow() {
-    if (nextCount === 12) {
-        format = "jpg";
-    } else {
-        format = "png";
-    }
-    banner.setAttribute("src", `./images/food${nextCount}.${format}`);
-    nextCount++;
-    if (nextCount === 14) {
-        nextCount = 1;
-    }
-}
+/**
+ * Switches images every 1 minute or 60 seconds
+ * @event { click }
+ * @function {{slideshow}}
+ */
+const slideshow = () => {
+	if (nextCount === 12) {
+		format = "jpg";
+	} else {
+		format = "png";
+	}
+	banner.setAttribute("src", `./images/food${nextCount}.${format}`);
+	nextCount++;
+	if (nextCount === 14) {
+		nextCount = 1;
+	}
+};
 setInterval(slideshow, 90000);
 banner.addEventListener("click", slideshow);
 
-//Open modal for order
-//Credit: Idea from Stackover flow
-window.onclick = function(){
-    let index;
-    for (index = 0; index < orderItem.length; ++index) {
-        orderItem[index].onclick = function(){
-            orderModal.style.display = "block";
-        };
-    };
+/**
+ * Open modal for item selection (add to cart)
+ * @event { click }
+ * @function {{Anonymous Function}}
+ */
+window.onclick = () => {
+	let index;
+	for (index = 0; index < orderItem.length; ++index) {
+		orderItem[index].onclick = function(){
+			orderModal.style.display = "block";
+		};
+	}
 };
 
-// close modal onclick outside for signup
-function closeOrderModal(m) {
-    if (m.target === orderModal) {
-        orderModal.style.display = "none";
-    };
+/**
+ * close modal onclick outside item selection (add to cart)
+ * @event { click }
+ * @function {{closeOrderModal}}
+ */
+const closeOrderModal = (m) => {
+	if (m.target === orderModal) {
+		orderModal.style.display = "none";
+	}
 };
 window.addEventListener("click", closeOrderModal);
 
-// close modal onclick the close button
+/**
+ * Close modal onclick the close button
+ * @event { click }
+ * @function {{Anonymous Function}}
+ */
 close.addEventListener("click", () => {
-    orderModal.style.display = "none";
+	orderModal.style.display = "none";
 });
 
+/**
+ * Navigates user to the index or landing page
+ * @event { onclick }
+ * @function {{Anonymous function}}{{Click event that targets the HTML id "logo"}}
+ */
 logo.onclick = () => {
-    window.location.assign("./index.html");
+	window.location.assign("./index.html");
 };
