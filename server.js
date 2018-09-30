35f5a983261a0a7bc582c041ca6cac80b16b0555
Nwabuzor C. Obiora <nwabuzor.obiora@gmail.com>
2018-09-30 21:55:04 +0100

    [ch-160843747] Install cors for client side

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import bodyParser from "body-parser";
 import logger from "morgan";
 import swaggerDocs from "./swaggerDocs.json";
@@ -8,6 +9,9 @@ import { orderRoutes, userRoutes, adminRoutes } from "./server/routes";
 const app = express();
 app.use(logger("dev"));
 
+// Enable All CORS Requests
+app.use(cors());
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/json" }));
@@ -16,6 +20,19 @@ app.use("/api-documentation", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 app.use(orderRoutes);
 app.use(userRoutes);
 app.use(adminRoutes);
+
+// Enable CORS from client-side
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "http://localhost:7000", "http://127.0.0.1:5500", "https://f-cube.herokuapp.com");
+	res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept, " +
+    "Authorization, Access-Control-Allow-Credentials"
+	);
+	res.header("Access-Control-Allow-Credentials", "true");
+	next();
+});
 
 app.get("/", (req, res) => {
 	res.status(200).json({
